a3ab7f09e55e4b74a3f1be96befe0714ffc2cb3a
Galbfran <francogalbiati984@gmail.com>
2023-09-29 17:53:29 -0300

    subo rutas CRUD funcionando

--- src/app/api/notes/[idNote]/route.ts
+++ src/app/api/notes/[idNote]/route.ts
@@ -1,19 +1,95 @@
 import { NextResponse } from "next/server"
+import {prisma} from "@/libs/prisma"
 
-export function GET(){
-    return NextResponse.json({
-        message: "pedir nota por id"
-    })
+interface Params {
+    params: {
+        idNote: String
+    }
+}
+export async function GET(request: Request , {params}: Params){
+    try {
+        const note = await prisma.note.findFirst({
+            where: {
+                id: Number(params.idNote)
+            }
+        })
+        if(note === null){
+            return NextResponse.json({
+                message: "nota no encontrada"
+            },{
+                status:404
+            })
+        }
+        return NextResponse.json(note)
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            },{
+                status:500
+            })
+        }
+    }
 }
 
-export function DELETE(){
-    return NextResponse.json({
-        message: "borrar nota por id"
-    })
+export async function DELETE(request: Request , {params}: Params){
+    try {
+        const noteDelete = await prisma.note.delete({
+            where: {
+                id: Number(params.idNote)
+            }
+        })
+        if(noteDelete === null){
+            return NextResponse.json({
+                message: "nota no encontrada"
+            },{
+                status:404
+            })
+        }
+        return NextResponse.json(noteDelete)
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            },{
+                status:500
+            })
+        }
+    }
 }
 
-export function PUT(){
-    return NextResponse.json({
-        message: "actualizar nota por id"
-    })
+
+
+
+
+
+export async function PUT(request: Request , {params}: Params){
+    const {title , content} = await request.json()
+    try {
+        const noteUpdate = await prisma.note.update({
+            where: {
+                id: Number(params.idNote)
+            },
+            data:{
+                title,
+                content
+            }
+        })
+        if(noteUpdate === null){
+            return NextResponse.json({
+                message: "nota no encontrada"
+            },{
+                status:404
+            })
+        }
+        return NextResponse.json(noteUpdate)
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            },{
+                status:500
+            })
+        }
+    }
 }
--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,13 +1,42 @@
 import { NextResponse } from "next/server";
+import {prisma} from "@/libs/prisma"
 
-export function GET() {
-    return NextResponse.json({
-        message: "debuelve todas las notas"
-    })
+export async function GET() {
+
+    try {
+        const notes = await prisma.note.findMany()
+        return NextResponse.json(notes)
+        
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            },{
+                status:500
+            })
+        }
+    }
 }
 
-export function POST() {
-    return NextResponse.json({
-        message: "creando nota"
-    })
+export async function POST(request : Request) {
+
+    const {title , content} = await request.json()
+    try {
+        const newNote = await prisma.note.create({
+            data: {
+                title,
+                content
+            }
+        })
+        return NextResponse.json(newNote)
+        
+    } catch (error) {
+        if (error instanceof Error){
+            return NextResponse.json({
+                message: error.message
+            },{
+                status:500
+            })
+        }
+    }
 }
